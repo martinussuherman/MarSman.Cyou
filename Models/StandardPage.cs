fdcb30a664b2842df56c8145919310cc3ca42f60
Martinus Suherman <martinus.suherman@gmail.com>
2020-12-19 12:23:30 +0700

    Code initial commit

---
+++ Models/StandardPage.cs
@@ -0,0 +1,10 @@
+using Piranha.AttributeBuilder;
+using Piranha.Models;
+
+namespace MarSman.Cyou.Models
+{
+    [PageType(Title = "Standard page")]
+    public class StandardPage  : Page<StandardPage>
+    {
+    }
+}
